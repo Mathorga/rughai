1bb4c0dcf5d829b4cf65918f7816fe4e178e6098
Luka Micheletti <michelettiluka@gmail.com>
2023-02-25 00:12:02 +0100

    Started sensor node

--- src/engine/sensor_node.py
+++ src/engine/sensor_node.py
@@ -19,12 +19,12 @@ class SensorNode(PositionNode):
 
         self.__scaling = scaling
 
-        self.__sprite = ShapeNode()
-        self.__sprite.scale = scaling
-        self.__sprite.push_handlers(self)
+        self.__shape = ShapeNode()
+        self.__shape.scale = scaling
+        self.__shape.push_handlers(self)
 
     def get_image(self):
-        return self.__sprite.image
+        return self.__shape.image
 
     def set_position(
         self,
@@ -33,11 +33,11 @@ class SensorNode(PositionNode):
     ) -> None:
         if x is not None:
             self.x = x
-            self.__sprite.x = x * self.__scaling
+            self.__shape.x = x * self.__scaling
 
         if y is not None:
             self.y = y
-            self.__sprite.y = y * self.__scaling
+            self.__shape.y = y * self.__scaling
 
     def set_scale(
         self,
@@ -45,36 +45,36 @@ class SensorNode(PositionNode):
         y_scale = None
     ) -> None:
         if x_scale is not None:
-            self.__sprite.scale_x = x_scale
+            self.__shape.scale_x = x_scale
 
         if y_scale is not None:
-            self.__sprite.scale_y = y_scale
+            self.__shape.scale_y = y_scale
 
     def set_image(self, image) -> None:
-        if image != None and (self.__sprite.image != image or (self.__sprite.image != None and self.__sprite.frame_index >= len(self.__sprite.image.frames) - 1)):
-            self.__sprite.image = image
+        if image != None and (self.__shape.image != image or (self.__shape.image != None and self.__shape.frame_index >= len(self.__shape.image.frames) - 1)):
+            self.__shape.image = image
 
     def on_animation_end(self):
         if self.__on_animation_end:
             self.__on_animation_end()
 
     def draw(self) -> None:
-        self.__sprite.draw()
+        self.__shape.draw()
 
     def get_bounding_box(self):
-        if isinstance(self.__sprite.image, pyglet.image.TextureRegion):
+        if isinstance(self.__shape.image, pyglet.image.TextureRegion):
             return (
-                self.__sprite.x - self.__sprite.image.anchor_x * self.__scaling,
-                self.__sprite.y - self.__sprite.image.anchor_y * self.__scaling,
-                self.__sprite.width,
-                self.__sprite.height
+                self.__shape.x - self.__shape.image.anchor_x * self.__scaling,
+                self.__shape.y - self.__shape.image.anchor_y * self.__scaling,
+                self.__shape.width,
+                self.__shape.height
             )
-        elif isinstance(self.__sprite.image, pyglet.image.animation.Animation):
+        elif isinstance(self.__shape.image, pyglet.image.animation.Animation):
             return (
-                self.__sprite.x - self.__sprite.image.frames[0].image.anchor_x * self.__scaling,
-                self.__sprite.y - self.__sprite.image.frames[0].image.anchor_y * self.__scaling,
-                self.__sprite.width,
-                self.__sprite.height
+                self.__shape.x - self.__shape.image.frames[0].image.anchor_x * self.__scaling,
+                self.__shape.y - self.__shape.image.frames[0].image.anchor_y * self.__scaling,
+                self.__shape.width,
+                self.__shape.height
             )
 
         return super().get_bounding_box()
