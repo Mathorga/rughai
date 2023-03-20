43d9a93ce6f1ae7bfbb34a75b59debfcc571d34f
Luka Micheletti <l.micheletti@ebw.it>
2023-03-20 11:13:53 +0100

    Added proper tile handling

--- src/engine/tilemap_node.py
+++ src/engine/tilemap_node.py
@@ -38,14 +38,17 @@ class Tileset:
                     tile = texture.get_region(x, texture.height - y - self.tile_height, self.tile_width, self.tile_height)
                     self.tiles.append(tile)
 
-                    # gl.glBindTexture(tile.target, tile.id)
+                    gl.glBindTexture(tile.target, tile.id)
+
+                    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+                    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
 
                     # # Set texture clamping to avoid mis-rendering subpixel edges.
                     # gl.glTexParameteri(tile.target, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
                     # gl.glTexParameteri(tile.target, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
                     # gl.glTexParameteri(tile.target, gl.GL_TEXTURE_WRAP_R, gl.GL_CLAMP_TO_EDGE)
 
-                    # gl.glBindTexture(tile.target, 0)
+                    gl.glBindTexture(tile.target, 0)
 
 class TilemapNode(PositionNode):
     def __init__(
