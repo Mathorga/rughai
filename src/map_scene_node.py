c5a83a079efccc417ba79e3ee1930648f50c6927
Luka Micheletti <michelettiluka@gmail.com>
2023-03-29 17:45:35 +0200

    Started prop load from file

---
+++ src/map_scene_node.py
@@ -0,0 +1,33 @@
+from typing import Callable, Optional
+import pyglet
+
+from engine.collision_manager import CollisionManager
+from engine.input_controller import InputController
+from engine.scene_manager_node import SceneManagerNode
+
+
+class MapSceneNode(SceneManagerNode):
+    """
+    Class defining a generic playable scene. It contains a player and accepts a prop placement file as input for placing props.
+    """
+    def __init__(
+            self,
+            window: pyglet.window.Window,
+            collision_manager: CollisionManager,
+            input_controller: InputController,
+            view_width: int,
+            view_height: int,
+            scaling: int = 1,
+            bundle: Optional[dict] = None,
+            on_ended: Optional[Callable[[dict], None]] = None
+    ) -> None:
+        super().__init__(
+            window,
+            collision_manager,
+            input_controller,
+            view_width,
+            view_height,
+            scaling,
+            bundle,
+            on_ended
+        )
